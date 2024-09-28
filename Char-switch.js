c1620afedca2b72e586134e0f1e1bf29a83bb577
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-09-28 08:33:40 +0200

    Update Char-switch.js

--- Char-switch.js
+++ Char-switch.js
@@ -20,3 +20,16 @@ function switcheroo(x){
 }
 
 // or
+
+function switcheroo(x){
+  let str = "";
+  for (let i = 0; i < x.length; i++) {
+      if (x[i] === 'a') {
+        str += 'b';
+      } else if (x[i] === 'b') {
+        str += 'a';
+      } else {
+        str += x[i];
+      }
+  } return str;
+}
